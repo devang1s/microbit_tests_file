fab8a9782f4c9bb0e346483055d1e35d2549545a
devang1s <97841937+devang1s@users.noreply.github.com>
2023-01-12 20:16:44 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+def askquestion():
+    question_no = 0
+    if question_no == 1:
+        basic.show_string("what is the capital of The UK")
+basic.show_string("A=London, B=Athens")
---
+++ main.ts
@@ -0,0 +1,9 @@
+function askquestion () {
+    let question_no = 0
+    if (question_no == 1) {
+        basic.showString("what is the capital of The UK")
+        basic.showString("A=London, B=Athens")
+    }
+}
+let colorbit_51bit: colorbit.Strip = null
+colorbit_51bit.drawColorBit(1, 1, colorbit.colors(BitColors.Green))

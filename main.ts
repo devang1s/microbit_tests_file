989b9f1f03fe2a3c1f0510cbd00f18168a5b1435
devang1s <97841937+devang1s@users.noreply.github.com>
2023-01-14 19:44:31 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    if (question_no == 1) {
+        basic.showIcon(IconNames.Yes)
+    }
+})
 function askquestion () {
     if (question_no == 1) {
         basic.showString("what is the capital of The UK")
         basic.showString("A=London, B=Athens")
     }
 }
-let question_no: colorbit.Strip = null
-question_no.drawColorBit(1, 1, colorbit.colors(BitColors.Green))
+input.onButtonPressed(Button.B, function () {
+    if (question_no == 1) {
+        basic.showIcon(IconNames.No)
+    }
+})
+let question_no = 0
+basic.showString("Hello!")
+question_no = 1
+askquestion()
